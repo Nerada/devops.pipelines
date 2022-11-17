680affc1a5fcaef224a8061df46078a8c4e1d362
Nerada <5610242+Nerada@users.noreply.github.com>
2022-11-17 10:04:24 +0100

    Update build script

--- build-scripts/buildMetaData.ps1
+++ build-scripts/buildMetaData.ps1
@@ -1,1 +1,22 @@
-echo "HelloWorldFromPs"
+param($officialBuild)
+
+if($officialBuild -ne "official-build")
+{
+	$user = $env:UserName 
+	$user += "|"
+}
+
+try
+{
+    git | Out-Null
+	
+	$branch = git symbolic-ref --short -q HEAD
+	$branch += "|"
+	$gitRef = git describe --always --abbrev=6 --dirty --exclude '*'
+	$gitRef += "|"
+}
+catch [System.Management.Automation.CommandNotFoundException]{}
+
+$buildTime = [System.DateTimeOffset]::Now.ToString("o")
+
+$user + $branch + $gitRef + $buildTime
